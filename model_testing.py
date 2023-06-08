9920088a2e57c63bcb4a205e01f6984add591e09
NataSonyaTasya <117571148+NataSonyaTasya@users.noreply.github.com>
2023-06-08 21:30:13 +0300

    Create model_testing.py

---
+++ model_testing.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+import os
+import glob
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures  # Класс преобразователь
+from sklearn import metrics
+#загрузка модели
+load_model = pickle.load(open(filename, 'rb'))
+df = pd.concat(map(pd.read_csv, glob.glob(os.path.join("test", "test*.csv"))), ignore_index= True)#загрузка всех тестовых данных в одну таблицу
+X_test=df['X_new']
+y_test= df['Y']
+poly = PolynomialFeatures(degree=3)
+X_poly = poly.fit_transform(X_test.to_numpy().reshape(-1, 1))       # Преобразуем данные
+y_pr=pr.predict(X_poly)
+print('среднеквадраточное отклонение : ', np.sqrt(metrics.mean_squared_error(y_test, y_pr)))
+f = open('score.txt','w')  # открытие в режиме записи
+f.write('среднеквадраточное отклонение : {}'.format(np.sqrt(metrics.mean_squared_error(y_test, y_pr))))  # запись 
+f.close()  # закрытие файла
