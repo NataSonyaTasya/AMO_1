34ce3f0134ee7e5802b2208e005ef773dfeec640
NataSonyaTasya <117571148+NataSonyaTasya@users.noreply.github.com>
2023-06-08 22:26:25 +0300

    Update model_testing.py

--- model_testing.py
+++ model_testing.py
@@ -18,5 +18,5 @@ X_poly = poly.fit_transform(X_test.to_numpy().reshape(-1, 1))       # Преоб
 y_pr=load_model.predict(X_poly)
 print('среднеквадраточное отклонение : ', np.sqrt(metrics.mean_squared_error(y_test, y_pr)))
 f = open('score.txt','w')  # открытие в режиме записи
-f.write('среднеквадраточное отклонение : {}'.format(np.sqrt(metrics.mean_squared_error(y_test, y_pr))))  # запись 
+f.write('root mean squared error: {}'.format(np.sqrt(metrics.mean_squared_error(y_test, y_pr))))  # запись 
 f.close()  # закрытие файла
